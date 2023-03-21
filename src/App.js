ce45fc357f01af13231897ee28ff74301aad5351
John Scharff <jscharff@ufl.edu>
2023-03-21 16:47:30 -0400

    Added Tailwind.css styling

--- src/App.js
+++ src/App.js
@@ -19,22 +19,25 @@ function App() {
 
   if (entryKey == 0) {
     page = 
-      <div>
-       <p>Welcome to RRM!</p>
-       <p onClick={entryAHandler}>Click here to access version A of the application!</p>
-       <p onClick={entryBHandler}>Click here to access version B of the application!</p>
+      <div class="flex flex-col justify-center items-center h-screen bg-blue-500">
+        <h1 class="text-white text-5xl font-bold mb-10">Welcome To</h1>
+        <h1 class="text-orange-400 text-6xl font-bold italic mb-10">RRM</h1>
+        <div class="flex space-x-4">
+          <div onClick={entryAHandler} class="bg-white text-blue-500 hover:bg-orange-400 hover:text-white transition ease-in-out duration-300 px-4 py-2 rounded-lg shadow-md">Design A</div>
+          <div onClick={entryBHandler} class="bg-white text-blue-500 hover:bg-orange-400 hover:text-white transition ease-in-out duration-300 px-4 py-2 rounded-lg shadow-md">Design B</div>
+        </div>
       </div>
   } else if (entryKey == 1) {
     page =
       <div>
-        <p>You're here!</p>
-        <p>We're now going to load design A!</p>
+        <h1>You're here!</h1>
+        <h3>We're now going to load design A!</h3>
       </div>;
   } else if (entryKey == 2) {
     page =
       <div>
-        <p>You're here!</p>
-        <p>We're now going to load design B!</p>
+        <h1>You're here!</h1>
+        <h3>We're now going to load design B!</h3>
       </div>
   }
 
