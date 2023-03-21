4bd828c2a78b7bb104985021de5d1633bcb83055
John Scharff <jscharff@ufl.edu>
2023-03-21 16:26:11 -0400

    App A & App B Decision Logic

--- src/App.js
+++ src/App.js
@@ -3,25 +3,38 @@ import './App.css';
 
 function App() {
 
-  const [entry, setEntry] = useState('')
+  const [entryKey, setEntryKey] = useState(0);
 
-  const clickHandler = (event) => {
-    setEntry(!entry);
+  const entryAHandler = (event) => {
+    setEntryKey(1);
   }
+
+  const entryBHandler = (event) => {
+    setEntryKey(2);
+  }
+
+  console.log(entryKey);
 
   let page;
 
-  if (entry) {
+  if (entryKey == 0) {
     page = 
       <div>
        <p>Welcome to RRM!</p>
-       <p onClick={clickHandler}>Click here to access the application!</p>
-      </div> 
-  } else {
+       <p onClick={entryAHandler}>Click here to access version A of the application!</p>
+       <p onClick={entryBHandler}>Click here to access version B of the application!</p>
+      </div>
+  } else if (entryKey == 1) {
     page =
       <div>
         <p>You're here!</p>
-        <p onClick={clickHandler}>Click here to go back!</p>
+        <p>We're now going to load design A!</p>
+      </div>;
+  } else if (entryKey == 2) {
+    page =
+      <div>
+        <p>You're here!</p>
+        <p>We're now going to load design B!</p>
       </div>
   }
 
