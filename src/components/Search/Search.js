ae14bc2d0dd9cdb539d0057f859ae8e7ef5f3073
Caroline Rogers <carolinerogers@ufl.edu>
2023-03-28 01:51:59 -0400

    Search + bubbles

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,15 +1,29 @@
 import React, { useState } from 'react';
 
-function Search() {
+function Search(props) {
 	const [restaurant, setRestaurant] = useState("");
   
+	const names = ["Panda Express", "Pollo Tropical", "Starbucks", "Mi Apa"];
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		alert(`The text you entered was ${restaurant}`)
+		
+		for(let i = 0; i < 4; i++) {
+			if(restaurant === names[i]) {
+				props.searchRestaurantTrigger(restaurant);
+			}
+		}
+		
+	}
+
 	return (
 		<div class="search">
 			<h1>Search</h1>
-			<form>
+			<form onSubmit={handleSubmit}>
 				<label>Search:
 					<input
-						type="text" 
+						type="text"
 						value={restaurant}
 						onChange={(e) => setRestaurant(e.target.value)}
 					/>
